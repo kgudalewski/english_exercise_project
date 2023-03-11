45b3755dd57ab78834e77319406b1a0a3efcedab
kgudalewski <114522279+kgudalewski@users.noreply.github.com>
2023-03-11 18:57:31 +0100

    Adding basic algorithm
    File contains English-Polish, Polish-English and random choice of both options to learn

---
+++ eng_exercises.py
@@ -0,0 +1,49 @@
+import random
+import pandas as pd
+
+df = pd.read_csv("slownik.csv", header=None, sep=";", encoding="cp1250", index_col=0).squeeze(axis=1)
+dict_of_words = df.to_dict()
+
+
+def eng_pol(number_of_words=0):
+    if number_of_words == 1:
+        eng_word = random.choice(list(dict_of_words))
+        pol_word = dict_of_words[eng_word]
+        print(eng_word, end=" - ")
+        if input():
+            print(pol_word.upper(), end="\n\n")
+    else:
+        while True:
+            eng_word = random.choice(list(dict_of_words))
+            pol_word = dict_of_words[eng_word]
+            print(eng_word, end=" - ")
+            if input():
+                print(pol_word.upper(), end="\n\n")
+
+
+def pol_eng(number_of_words=0):
+    if number_of_words == 1:
+        eng_word = random.choice(list(dict_of_words))
+        pol_word = dict_of_words[eng_word]
+        print(pol_word, end=" - ")
+        if input():
+            print(eng_word.upper(), end="\n\n")
+    else:
+        while True:
+            eng_word = random.choice(list(dict_of_words))
+            pol_word = dict_of_words[eng_word]
+            print(pol_word, end=" - ")
+            if input():
+                print(eng_word.upper(), end="\n\n")
+
+
+def random_choice():
+    while True:
+        if random.randint(0, 1) == 0:
+            eng_pol(1)
+        else:
+            pol_eng(1)
+
+
+if __name__ == "__main__":
+    random_choice()
