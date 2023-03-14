bbb7014477f88ddc90fd5a739de1f8e8b67309e4
kgudalewski <114522279+kgudalewski@users.noreply.github.com>
2023-03-14 11:20:18 +0100

    Added functionality: taking word from x% of the hardest words
    Functionality make studying more efficient, it sorts dataframe by weights (translation's difficulty) and randomly choose word from x% of the hardest words.

--- eng_exercises.py
+++ eng_exercises.py
@@ -1,63 +1,82 @@
 import random
 import pandas as pd
 import warnings
+import sys
 warnings.filterwarnings('ignore')
 
 df = pd.read_csv("dictionary.csv", index_col=0)
 
+print("Hello in English learning app !")
+print("-------------------------------")
+print("Type 'exit' for correct question to save and exit the game")
+print("-------------------------------")
+print("Let's start the game !!", end="\n\n")
 
-def upload_weight(dframe, indx):
+
+def sort_df_by_weight():
+    global df
+    df = df.sort_values(by="weights").reset_index(drop=True)
+
+
+def check_func(idx):
+    global df
     yes_rate = 1.3
     no_rate = 0.7
-    if (input("Correct ? [Y/N] ").upper()) == "Y":
-        dframe.weights[indx] *= yes_rate
-    else:
-        dframe.weights[indx] *= no_rate
+    answer = input("Correct ? [Y/N] ").upper()
+    if answer == "Y":
+        df.weights[idx] *= yes_rate
+        sort_df_by_weight()
+    elif answer == "N":
+        df.weights[idx] *= no_rate
+        sort_df_by_weight()
+    elif answer == "EXIT":
+        df.to_csv("dictionary.csv")
+        sys.exit()
     print()
 
 
 def eng_pol(number_of_words=0):
+
+    percent_of_words = 0.2
     if number_of_words == 1:
-        idx = random.randint(0, df.shape[0])
+        idx = random.randint(0, int(round(df.shape[0] * percent_of_words)))
         eng_word = df.ANG[idx]
         pol_word = df.POL[idx]
         print(eng_word, end=" - ")
         if input():
             print(pol_word.upper())
-            upload_weight(df, idx)
-            df.to_csv("dictionary.csv")
+            check_func(idx)
+
     else:
         while True:
-            idx = random.randint(0, df.shape[0])
+            idx = random.randint(0, int(round(df.shape[0] * percent_of_words)))
             eng_word = df.ANG[idx]
             pol_word = df.POL[idx]
             print(eng_word, end=" - ")
             if input():
                 print(pol_word.upper())
-                upload_weight(df, idx)
-                df.to_csv("dictionary.csv")
+                check_func(idx)
 
 
 def pol_eng(number_of_words=0):
+    percent_of_words = 0.2
     if number_of_words == 1:
-        idx = random.randint(0, df.shape[0])
+        idx = random.randint(0, int(round(df.shape[0] * percent_of_words)))
         eng_word = df.ANG[idx]
         pol_word = df.POL[idx]
         print(pol_word, end=" - ")
         if input():
             print(eng_word.upper())
-            upload_weight(df, idx)
-            df.to_csv("dictionary.csv")
+            check_func(idx)
     else:
         while True:
-            idx = random.randint(0, df.shape[0])
+            idx = random.randint(0, int(round(df.shape[0] * percent_of_words)))
             eng_word = df.ANG[idx]
             pol_word = df.POL[idx]
             print(pol_word, end=" - ")
             if input():
                 print(eng_word.upper())
-                upload_weight(df, idx)
-                df.to_csv("dictionary.csv")
+                check_func(idx)
 
 
 def random_choice():
@@ -69,4 +88,4 @@ def random_choice():
 
 
 if __name__ == "__main__":
-    eng_pol()
+    random_choice()
